70f62bfef95449a9e389164faa803faf9ee8e8db
vschaitu <vschaitu@gmail.com>
2018-06-22 16:41:45 -0500

    Adding JawsDb correct connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection ;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
+  user: "root", 
   password: "",
   database: "burgers_db"
 });
